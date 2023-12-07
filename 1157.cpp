94198abca4ff1368684128153a2b98a890ca4a94
tthungjun <150824735+tthungjun@users.noreply.github.com>
2023-12-07 15:30:34 +0900

    Update 1157.cpp

--- 1157.cpp
+++ 1157.cpp
@@ -1,55 +1,58 @@
 #include <iostream>
+#include <unordered_map>
 #include <string>
-#include <algorithm>
+#include <cctype>
+
 using namespace std;
 
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-	string word;
-	cin >> word;
+    string word;
+    cin >> word;
 
-	string result;
+    unordered_map<char, int> charCount;
 
-	for (int i = 0; i < word.length(); i++)
-	{
-		char ch = word[i];
+    for (char ch : word)
+    {
+        if (islower(ch))
+        {
+            ch = toupper(ch);
+        }
+        charCount[ch]++;
+    }
 
-		if (ch >= 97 && ch < 123)
-		{
-			word[i] = ch - 32;
-		}
-	}
+    char result = '?';
+    int maxCount = 0;
+    bool isDuplicate = false;
 
-	for (char& ch : word)
-	{
-		if (islower(ch))
-			ch = toupper(ch);
-	}
+    for (const auto& entry : charCount)
+    {
+        // entry.first는 키이다. 즉, 여기서는 char 타입의 단어의 문자
+        // entry.second는 값이다. 즉, 여기서는 char의 등장 횟수
+        if (entry.second > maxCount)
+        {
+            maxCount = entry.second;
+            result = entry.first;
+            isDuplicate = false;
+        }
+        else if (entry.second == maxCount)
+        {
+            isDuplicate = true;
+        }
+    }
 
-	int num = count(word.begin(), word.end(), word[0]);
-	result = word[0];
+    if (isDuplicate)
+    {
+        cout << "?";
+    }
+    else
+    {
+        cout << result;
+    }
 
-	for (int i = 0; i < word.length(); i++)
-	{
-		if (count(word.begin(), word.end(), word[i]) > num)
-		{
-			num = count(word.begin(), word.end(), word[i]);
-			result = word[i];
-		}
-		else if (count(word.begin(), word.end(), word[i]) == num)
-		{
-			if (result == string(1, word[i]))
-				continue;
-			else
-				result = "?";
-		}
-	}
-
-	cout << result;
-
-	return 0;
+    return 0;
 }
