7e9fd52f65bd8749a2031e23d5a457c83bcaee49
tthungjun <150824735+tthungjun@users.noreply.github.com>
2024-01-13 16:12:25 +0900

    Update AC.cc

--- 백준/Gold/5430. AC/AC.cc
+++ 백준/Gold/5430. AC/AC.cc
@@ -5,6 +5,8 @@
 #include <vector>
 using namespace std;
 
+// 덱의 구조를 이용한 인덱스 이용
+
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
@@ -88,3 +90,77 @@ int main() {
 
 	return 0;
 }
+
+// 시간 초과 발생한 내 첫 코드
+
+/*#include <iostream>
+#include <deque>
+#include <string>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int _case;
+	cin >> _case;
+
+	vector<string> ans;
+	for (int i = 0; i < _case; i++) {
+		string func;
+		cin >> func;
+
+		int size;
+		cin >> size;
+
+		string arr;
+		cin >> arr;
+
+		deque<string> num;
+		stringstream sstream(arr.substr(1, arr.size() - 2));
+		string a;
+
+		while (getline(sstream, a, ',')) {
+			num.push_back(a);
+		}
+
+		int count = 0;
+		for (int j = 0; j < func.size(); j++) {
+			if (func[j] == 'R') {
+				for (int k = num.size() - 2; k >= 0; k--) {
+					string n = num[k];
+					num.push_back(n);
+				}
+				for (int k = 0; k < num.size() - 1; k++) {
+					num.pop_front();
+				}
+			}
+			else if (func[j] == 'D') {
+				if (num.empty()) {
+					cout << "error" << '\n';
+					count++;
+					break;
+				}
+				else num.pop_front();
+			}
+		}
+
+		if (count == 1) {
+			continue;
+		}
+
+		if (!num.empty()) {
+			cout << "[";
+			for (int i = 0; i < num.size() - 1; i++) {
+				cout << num[i] << ",";
+			}
+			cout << num[num.size() - 1] << "]\n";
+		}
+		else cout << "[]\n";
+	}
+
+	return 0;
+}*/
