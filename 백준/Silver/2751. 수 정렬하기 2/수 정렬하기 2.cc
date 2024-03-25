56d738fed0c746d566a495ade5c33526b293fc17
tthungjun <150824735+tthungjun@users.noreply.github.com>
2024-03-25 13:11:06 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 272 ms, Memory: 9836 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -1,62 +1,28 @@
 #include <iostream>
-#include <vector>
+#include<vector>
 using namespace std;
 
-void merge_sort(int s, int e);
-static vector<int> v;
-static vector<int> temp;
-
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL);
+	cout.tie(NULL);
 
-    int n;
-    cin >> n;
-    v = vector<int>(n + 1, 0);
-    temp = vector<int>(n + 1, 0);
+	long n;
+	cin >> n;
 
-    for (int i = 1; i <= n; i++) {
-        cin >> v[i];
-    }
+	vector<int> arr(2000001, 0);
+	for (int i = 0; i < n; i++) {
+		int a;
+		cin >> a;
+		arr[a+1000000] += 1;
+	}
 
-    merge_sort(1, n);
+	for (int i = 0; i < 2000002; i++) {
+		if (arr[i] > 0) {
+			for (int j = arr[i]; j > 0; j--)
+				cout << i - 1000000 << '\n';
+		}
+	}
 
-    for (int i = 1; i <= n; i++) {
-        cout << v[i] << '\n';
-    }
-
-    return 0;
-}
-
-void merge_sort(int s, int e) {
-    if (s < e) {
-        int m = s + (e - s) / 2;
-        merge_sort(s, m);
-        merge_sort(m + 1, e);
-
-        int i = s;
-        int j = m + 1;
-        int k = s;
-
-        while (i <= m && j <= e) {
-            if (v[i] <= v[j]) {
-                temp[k++] = v[i++];
-            } else {
-                temp[k++] = v[j++];
-            }
-        }
-
-        while (i <= m) {
-            temp[k++] = v[i++];
-        }
-
-        while (j <= e) {
-            temp[k++] = v[j++];
-        }
-
-        for (int idx = s; idx <= e; idx++) {
-            v[idx] = temp[idx];
-        }
-    }
+	return 0;
 }
